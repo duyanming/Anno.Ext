a272959680298998fdcb5be6bca07722c66f79ad
duyanming <dym880@163.com>
2019-11-08 15:30:15 +0800

    初始化

---
+++ Anno.EngineData.ExtLRUCache/CacheLRU.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Anno.EngineData
+{
+    using Anno.EngineData.Cache;
+    using Anno.LRUCache;
+    public class CacheLRU : CacheMiddlewareAttribute
+    {
+        public CacheLRU()
+        {
+            cacheLRU = new LRUCache<int, ActionResult>(Capacity, ExpireSeconds);
+        }
+        public bool IsGetAndResetTime { get; set; } = true;
+        public int Capacity { get; set; } = 255;
+        public double ExpireSeconds { get; set; } = 10 * 60;
+        LRUCache<int, ActionResult> cacheLRU;
+        public override void RemoveCache(int key)
+        {
+            cacheLRU.Remove(key);
+        }
+
+        public override void SetCache(int key, ActionResult actionResult)
+        {
+            cacheLRU.Set(key, actionResult);
+        }
+
+        public override bool TryGetCache(int key, out ActionResult actionResult)
+        {
+            return cacheLRU.TryGet(key, out actionResult);
+        }
+    }
+}
---
+++ Anno.LRUCache/LRUCache.cs
@@ -0,0 +1,259 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anno.LRUCache
+{
+    /// <summary>
+    /// 默认缓存长度255 五秒检查一次 30分钟未访问的 销毁
+    /// 超过总长度的会被销毁，超时未访问的也销毁
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class LRUCache<TKey, TValue> : IDisposable
+    {
+        private bool disposed;
+        private CancellationTokenSource cancelToken;
+        const int DEFAULT_CAPACITY = 255;
+
+        int _capacity;
+        double _seconds = 60 * 30;//30分钟
+        ReaderWriterLockSlim _locker;
+        IDictionary<TKey, TValue> _dictionary;
+        /// <summary>
+        /// Key 最后访问时间
+        /// </summary>
+        IDictionary<TKey, DateTime> _keyLastVisitTimeDictionary;
+        /// <summary>
+        /// Key 链表
+        /// </summary>
+        public LinkedList<TKey> _linkedList;
+        public LRUCache() : this(DEFAULT_CAPACITY) { }
+        ~LRUCache()
+        {
+            Dispose(false);
+        }
+
+        public LRUCache(int capacity)
+        {
+            _locker = new ReaderWriterLockSlim();
+            _capacity = capacity > 0 ? capacity : DEFAULT_CAPACITY;
+            _dictionary = new Dictionary<TKey, TValue>();
+            _keyLastVisitTimeDictionary = new Dictionary<TKey, DateTime>();
+            _linkedList = new LinkedList<TKey>();
+            cancelToken = new CancellationTokenSource();
+            Expire();
+        }
+        public LRUCache(int capacity, double expireSeconds) : this(capacity)
+        {
+            if (expireSeconds > 0)
+            {
+                _seconds = expireSeconds;
+            }
+        }
+
+        public void Set(TKey key, TValue value)
+        {
+            _locker.EnterWriteLock();
+            try
+            {
+                _dictionary[key] = value;
+                _keyLastVisitTimeDictionary[key] = DateTime.Now;
+                _linkedList.Remove(key);
+                _linkedList.AddFirst(key);
+                if (_linkedList.Count > _capacity)
+                {
+                    _dictionary.Remove(_linkedList.Last.Value);
+                    _keyLastVisitTimeDictionary.Remove(_linkedList.Last.Value);
+                    _linkedList.RemoveLast();
+                }
+            }
+            finally { _locker.ExitWriteLock(); }
+        }
+        /// <summary>
+        /// 获取Value 自定义是否延长 有效期
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="isupdate">true  延长有效期; false 不延长有效期</param>
+        /// <returns></returns>
+        public bool TryGet(TKey key, out TValue value, bool isupdate)
+        {
+            _locker.EnterUpgradeableReadLock();
+            try
+            {
+                bool b = _dictionary.TryGetValue(key, out value);
+                if (b)
+                {
+                    _locker.EnterWriteLock();
+                    try
+                    {
+                        if (isupdate)
+                        {
+                            _keyLastVisitTimeDictionary[key] = DateTime.Now;
+                        }
+                        _linkedList.Remove(key);
+                        _linkedList.AddFirst(key);
+                    }
+                    finally { _locker.ExitWriteLock(); }
+                }
+                return b;
+            }
+            catch { throw; }
+            finally { _locker.ExitUpgradeableReadLock(); }
+        }
+        /// <summary>
+        /// 获取Value 并延长 有效期
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool TryGet(TKey key, out TValue value)
+        {
+            var rlt = TryGet(key, out TValue tvalue, true);
+            value = tvalue;
+            return rlt;
+        }
+        /// <summary>
+        /// 根据指定Key 移除 缓存
+        /// </summary>
+        /// <param name="key"></param>
+        public void Remove(TKey key)
+        {
+            _locker.EnterWriteLock();
+            try
+            {
+                _dictionary.Remove(key);
+                _keyLastVisitTimeDictionary.Remove(key);
+                _linkedList.Remove(key);
+            }
+            finally
+            {
+                _locker.ExitWriteLock();
+            }
+        }
+        public bool ContainsKey(TKey key)
+        {
+            _locker.EnterReadLock();
+            try
+            {
+                return _dictionary.ContainsKey(key);
+            }
+            finally { _locker.ExitReadLock(); }
+        }
+
+        public int Count
+        {
+            get
+            {
+                _locker.EnterReadLock();
+                try
+                {
+                    return _dictionary.Count;
+                }
+                finally { _locker.ExitReadLock(); }
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                _locker.EnterReadLock();
+                try
+                {
+                    return _capacity;
+                }
+                finally { _locker.ExitReadLock(); }
+            }
+            set
+            {
+                _locker.EnterUpgradeableReadLock();
+                try
+                {
+                    if (value > 0 && _capacity != value)
+                    {
+                        _locker.EnterWriteLock();
+                        try
+                        {
+                            _capacity = value;
+                            while (_linkedList.Count > _capacity)
+                            {
+                                _linkedList.RemoveLast();
+                            }
+                        }
+                        finally { _locker.ExitWriteLock(); }
+                    }
+                }
+                finally { _locker.ExitUpgradeableReadLock(); }
+            }
+        }
+
+        public ICollection<TKey> Keys
+        {
+            get
+            {
+                _locker.EnterReadLock();
+                try
+                {
+                    return _dictionary.Keys;
+                }
+                finally { _locker.ExitReadLock(); }
+            }
+        }
+
+        public ICollection<TValue> Values
+        {
+            get
+            {
+                _locker.EnterReadLock();
+                try
+                {
+                    return _dictionary.Values;
+                }
+                finally { _locker.ExitReadLock(); }
+            }
+        }
+
+        private void Expire()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                while (cancelToken.Token.IsCancellationRequested == false)
+                {
+                    var keys = _keyLastVisitTimeDictionary.Keys.ToList();
+                    var now = DateTime.Now;
+                    foreach (var key in keys)
+                    {
+                        _keyLastVisitTimeDictionary.TryGetValue(key, out DateTime dateTime);
+                        if (dateTime != null && (now - dateTime).TotalSeconds > _seconds)
+                        {
+                            Remove(key);
+
+                        }
+                    }
+                    Thread.Sleep(5000);
+                }
+            }, cancelToken.Token);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    cancelToken.Cancel();
+                }
+                disposed = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
